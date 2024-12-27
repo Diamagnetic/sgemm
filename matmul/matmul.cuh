9de81f308c38bab1efe321de8993b199ffa4ec8e
Chirag Dhamange <c.dhamange@gmail.com>
2024-12-27 11:18:05 -0800

    Updated CUDA kernel header file

--- matmul/matmul.cuh
+++ matmul/matmul.cuh
@@ -1,7 +1,21 @@
-#ifndef MATMUL_H
-#define MATMUL_H
-void sgemm(int M, int N, int K, float alpha,
-           const float A*, const float B*,
-           float beta, float *C
-           );
+#ifndef MATMUL_CUDA_H
+#define MATMUL_CUDA_H
+__global__ void sgemm_naive(int M, int N, int K, float alpha,
+    const float *A, const float *B, float beta, float *C
+    );
+
+void run_mat_mul(const int &M, const int &N, const int &K,
+    const int &alpha, float *A, float *B,
+    const int &beta, float *C
+    );
+
+void alloc_space(float **A, float **B, float **C, const int &M,
+    const int &N, const int &K
+    );
+
+void init_mat(float *A, float *B, float *C, const int &M,
+    const int &N, const int &K
+    );
+
+void free_space(float *A, float *B, float *C);
 #endif
