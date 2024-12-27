70e81f94cc66b2b570ca0b58bfbacbf90ec062db
Chirag Dhamange <c.dhamange@gmail.com>
2024-12-27 14:24:05 -0800

    Added comments

--- matmul/matmul.cu
+++ matmul/matmul.cu
@@ -1,10 +1,31 @@
+/*
+ * Author: Chirag Dhamange
+ * File Name: matmul.cu
+ * Purpose: CUDA code for Standard GEneral Matrix Multiplication (SGEMM).
+ *          The following techniques were used and performance was
+ *          measured:
+ *          1. Naive
+ *
+ * C = beta*C + alpha*A*B, where A, B, and C are matrices, and
+ * alpha, and beta are scalars
+ *
+ * Referenced from: https://siboehm.com/articles/22/CUDA-MMM
+*/
+
 #include "matmul.cuh"
 
+// The kernel performs SGEMM on two matrices
+// A particular thread calculates the result of one cell in
+// the resultant matrix
+//
+// C = beta*C + alpha*A*B, where A, B, and C are matrices, and
+// alpha, and beta are scalars
 __global__ void sgemm_naive(int M, int N, int K, float alpha,
     const float *A, const float *B,
     float beta, float *C
     )
 {
+  // access matrix entry (row = x, col = y) in matrix C
   const uint x = blockDim.x * blockIdx.x + threadIdx.x;
   const uint y = blockDim.y * blockIdx.y + threadIdx.y;
 
@@ -12,6 +33,7 @@ __global__ void sgemm_naive(int M, int N, int K, float alpha,
   {
     float tmp = 0.0;
 
+    // multiply row = x of A with col = y of B
     for(int i = 0; i < K; i++)
       tmp += A[x * K + i] * B[i * N + y];
 
@@ -20,6 +42,8 @@ __global__ void sgemm_naive(int M, int N, int K, float alpha,
   }
 }
 
+// A wrapper function that sets the dimensions of the grid and block
+// and executes the kernel
 void run_mat_mul(const int &M, const int &N, const int &K,
     const int &alpha, float *A, float *B,
     const int &beta, float *C
@@ -32,6 +56,7 @@ void run_mat_mul(const int &M, const int &N, const int &K,
   cudaDeviceSynchronize();
 }
 
+// Allocate space for the matrices on the device
 void alloc_space(float **A, float **B, float **C, const int &M,
     const int &N, const int &K
     )
@@ -41,6 +66,7 @@ void alloc_space(float **A, float **B, float **C, const int &M,
   cudaMalloc(C, M * N);
 }
 
+// Set all the cells in the matrices to a number
 void init_mat(float *A, float *B, float *C, const int &M,
     const int &N, const int &K
     )
@@ -50,6 +76,7 @@ void init_mat(float *A, float *B, float *C, const int &M,
   cudaMemset(C, 1, M * N);
 }
 
+// Free the allocated space
 void free_space(float *A, float *B, float *C)
 {
   cudaFree(A);
